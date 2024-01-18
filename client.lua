82b12ba68c983ca75a57c73c1fc4953b9c1876e8
Pablo Rodríguez <85810125+pablordgez@users.noreply.github.com>
2024-01-18 23:45:47 +0100

    Added functionality to remove spawned props

--- client.lua
+++ client.lua
@@ -1,6 +1,12 @@
 _menuPool = NativeUI.CreatePool()
 mainMenu = NativeUI.CreateMenu("Prop menu", "~b~Spawn props")
 _menuPool:Add(mainMenu)
+local spawnMenu = _menuPool:AddSubMenu(mainMenu, "Spawn props")
+local removeMenu = _menuPool:AddSubMenu(mainMenu, "Remove props")
+local noProps = NativeUI.CreateItem("No props spawned", "No props spawned")
+local spawnedProps = {}
+removeMenu:AddItem(noProps)
+local menuIndex = 0
 _menuPool:MouseEdgeEnabled(false)
 
 function createObject(id)
@@ -27,12 +33,49 @@ function bringObject(entity)
     SetEntityCoords(entity, GetEntityCoords(PlayerPedId()), false, false, false, false)
 end
 
-function spawnObject(id)
+function spawnObject(id, name)
     local object = createObject(id)
     reduceOpacity(object)
     disableCollisions(object)
     bringObject(object)
+    
     return object
+end
+
+function createRemoveMenu(object, name)
+    local submenu = _menuPool:AddSubMenu(removeMenu, name)
+    if menuIndex == 0 then
+        removeMenu:RemoveItemAt(1)
+    end
+    _menuPool:MouseEdgeEnabled(false)
+    local yes = NativeUI.CreateItem("Yes", "Remove the prop")
+    local no = NativeUI.CreateItem("No", "Go back")
+    menuIndex = menuIndex + 1
+    
+    submenu:AddItem(yes)
+    submenu:AddItem(no)
+    submenu.OnItemSelect = function(sender, item, index)
+        if item == yes then
+            deleteObject(object)
+            menuIndex = menuIndex - 1
+            submenu:GoBack()
+            if menuIndex == 0 then
+                removeMenu:AddItem(noProps)
+            end
+            local submenuIndex = 0
+            for i=1, #spawnedProps do
+                if spawnedProps[i] == object then
+                    submenuIndex = i
+                    break
+                end
+            end
+            removeMenu:RemoveItemAt(submenuIndex)
+            table.remove(spawnedProps, submenuIndex)
+            _menuPool:RefreshIndex()
+        else
+            submenu:GoBack()
+        end
+    end
 end
 
 function setObject(entity, hasPhysics)
@@ -74,8 +117,8 @@ local items = {
 }
 
 
-function addSubmenu(menu, object)
-    local submenu = _menuPool:AddSubMenu(menu, items[object][1])
+function addSubmenu(object)
+    local submenu = _menuPool:AddSubMenu(spawnMenu, items[object][1])
     _menuPool:MouseEdgeEnabled(false)
     local spawn = NativeUI.CreateItem("Spawn", "Spawn the prop")
     local rotate = NativeUI.CreateItem("Rotate", "Rotate the prop")
@@ -91,7 +134,7 @@ function addSubmenu(menu, object)
     submenu:AddItem(finish)
     submenu.OnItemSelect = function(sender, item, index)
         if item == spawn and objectcreated == nil then
-            objectcreated = spawnObject(items[object][2])
+            objectcreated = spawnObject(items[object][2], items[object][1])
         elseif item == rotate and objectcreated ~= nil then
             rotateObject(objectcreated)
         elseif item == moveup and objectcreated ~= nil then
@@ -109,6 +152,8 @@ function addSubmenu(menu, object)
             deleteObject(objectcreated)
         else
             spawned = false
+            table.insert(spawnedProps, objectcreated)
+            createRemoveMenu(objectcreated, items[object][1])
             
         end
         objectcreated = nil
@@ -116,7 +161,7 @@ function addSubmenu(menu, object)
 end
 
 for key, value in pairs(items) do
-    addSubmenu(mainMenu, key)
+    addSubmenu(key)
 end
 
 _menuPool:RefreshIndex()
